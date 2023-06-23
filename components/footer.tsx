c906cf985f3d28988eec5d70017f8903b8021a70
Prasanna Tuladhar <tuladharprasan@gmail.com>
2023-06-23 18:02:01 +0200

    UI changes in Homepage

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,10 +9,10 @@ import Image from 'next/image'
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="bg-white dark:bg-zinc-900 border-t border-accent-2">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+          <h3 className="text-4xl text-black dark:text-white lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Follow us on social media.
           </h3>
           <div className="flex flex-row justify-end lg:pl-4 lg:w-1/2">
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,28 @@ import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
-import { useEffect, useState, createRef } from 'react'
+import { useEffect, useState, createRef,useRef } from 'react'
 import cover from '../public/images/cover.png'
+// Import Slick React components
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Link from 'next/link'
 import Slider from "react-slick";
+// Import Swiper React components
+import { Swiper, SwiperSlide,useSwiper } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import  { Grid, Pagination, Navigation } from "swiper";
+
 import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/24/outline'
+
+
 
 const images = [{id:1,img:'https://images.unsplash.com/photo-1506501139174-099022df5260?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80'},{id:2,img:'https://images.unsplash.com/photo-1523438097201-512ae7d59c44?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'},{id:3,img:'https://images.unsplash.com/photo-1513026705753-bc3fffca8bf4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'},{id:4,img:'https://images.unsplash.com/photo-1523438097201-512ae7d59c44?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'},{id:5,img:'https://images.unsplash.com/photo-1513026705753-bc3fffca8bf4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'} ]
 
@@ -46,21 +61,37 @@ const CustomPrevArrow = (props) => {
   );
 };
 
+
+
+
 export default function Index({ allPosts: { edges }, preview }) {
+ 
   const heroPost = edges[1]?.node
   const morePosts = edges
 
-  useEffect(()=>{
-console.log(morePosts)
-  },[])
+  const swiperRef = useRef(null);
+
+  const goPrev = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+
+  const goNext = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
 
   const settings = {
     className: "center",
     centerMode: true,
     infinite: true,
     pagination: true,
+    // autoplay: true,
     slidesToShow: 4,
     centerPadding: "60px",
+    pauseOnHover: true,
     responsive: [
       {
         breakpoint: 1280,
@@ -108,34 +139,6 @@ console.log(morePosts)
               The largest community <br/>of TEDx enthusiasts
             </article>
             </div>
-            {/* <div  className='flex-1 '/> */}
-           
-            
-            {/* <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-slate-400">
-              Learn about ideas worth spreading by attending our <span className='text-red-600 font-bold'>TEDx</span> events, watching our <span className='text-red-600 font-bold'>TEDx</span> talks and reading our <span className='text-red-600 font-bold'>TEDx</span> blogs.
-            </p> */}
-            {/* <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div> */}
-         
-          {/* <div className="hidden sm:mt-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of talks.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Watch <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div> */}
-       
     
       </div>
       <div className="w-full  bg-red-600 text-center py-4 text-white">
@@ -159,78 +162,143 @@ console.log(morePosts)
         </div>
       </div>
       
-      <div >
+      <div>
         <div className="mt-14 px-8 md:px-14 ">
             <h1 className=" text-xl md:text-2xl  text-gray-900 dark:text-slate-100 ">
              Most seen videos <span className='text-red-500 text-lg ml-5'>Explore now</span>
             </h1>
         </div>
         <div className="my-10 ">
-            
-            {/* <Swiper
-      // install Swiper modules
-  
-      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-      spaceBetween={50}
-      // slidesPerView={3}
-      navigation
-      className="mySwiper"
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      breakpoints={{
-        // Configure breakpoints based on screen width
-        0: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
-      }}
-    > */}
+           
     {morePosts && (
       <Slider {...settings}>
     {morePosts.map((slideContent, index) => (
         <div className='p-3'>
            <Link
               href={`/posts/${slideContent.node.slug}`}
-              // className="hover:underline"
+              className="flex flex-col"
               // dangerouslySetInnerHTML={{ __html: title }}
             >
           <img
             key={index}
             src={slideContent.node.featuredImage.node.sourceUrl}
             
-            className="h-20 md:h-40 lg:h-60  object-cover overflow-hidden  "
+            className="h-40 md:h-40 lg:h-60  object-cover overflow-hidden  "
           />
           
-          <div className='text-black dark:text-white text-center justify-center py-4'>{slideContent.node.title}</div>
+          <div className='text-black dark:text-white text-center justify-center py-4 text-xs md:text-sm lg:text-lg'>{slideContent.node.title}</div>
           </Link>
         </div>
       ))}
     </Slider>
     )}
     
-       
-     
     {/* </Swiper> */}
     </div>
-        {heroPost && (
-          <HeroPost
-            title={heroPost.title}
-            coverImage={heroPost.featuredImage}
-            date={heroPost.date}
-            author={heroPost.author}
-            slug={heroPost.slug}
-            excerpt={heroPost.excerpt}
+
+{/* Speakers swiper */}
+<div> <div className="mt-14 ">
+            <h1 className=" text-xl md:text-2xl  text-gray-900 dark:text-slate-100 text-center">
+             Our Speakers 
+            </h1>
+        </div>
+        
+       
+       <div>
+         
+    
+      
+        <div className="px-10 md:px-20 py-10 relative">
+        <button
+     onClick={goPrev}
+     className="absolute left-3 top-1/3"
+    >
+      <ChevronLeftIcon className="text-black dark:text-white h-5 lg:h-10 w-5 lg:w-10 bg-red-600 rounded-lg" />
+    </button>
+    <button
+     
+     onClick={goNext}
+     className="absolute right-3 top-1/3"
+    >
+    
+      <ChevronRightIcon className="text-black dark:text-white h-5 lg:h-10 w-5 lg:w-10 bg-red-600 rounded-lg" />
+     
+    </button>
+       
+        <Swiper
+    //  navigation={true}
+        slidesPerView={4}
+        grid={{
+          rows: 2,
+          fill:"row"
+        }}
+        
+        ref={swiperRef}
+        spaceBetween={20}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Grid, Pagination,Navigation]}
+        className="mySwiper"
+      >
             
-          />
-        )}
-        {morePosts.length > 0 && <MoreStories posts={morePosts} title={'More Events'} />}
+         <SwiperSlide><div className='py-10 md:py-28 bg-black dark:bg-white'>Slide 1</div></SwiperSlide>
+        <SwiperSlide><div className='py-10 md:py-28 bg-black dark:bg-white'>Slide 2</div></SwiperSlide>
+        <SwiperSlide><div className='py-10 md:py-28 bg-black dark:bg-white'>Slide 3</div></SwiperSlide>
+        <SwiperSlide><div className='py-10 md:py-28 bg-black dark:bg-white'>Slide 4</div></SwiperSlide>
+        <SwiperSlide><div className='py-10 md:py-28 bg-black dark:bg-white'>Slide 5</div></SwiperSlide>
+        <SwiperSlide><div className='py-10 md:py-28 bg-black dark:bg-white'>Slide 6</div></SwiperSlide>
+        <SwiperSlide><div className='py-10 md:py-28 bg-black dark:bg-white'>Slide 7</div></SwiperSlide>
+        <SwiperSlide><div className='py-10 md:py-28 bg-black dark:bg-white'>Slide 8</div></SwiperSlide>
+        <SwiperSlide><div className='py-10 md:py-28 bg-black dark:bg-white'>Slide 9</div></SwiperSlide>
+        
+      </Swiper>
+
+    </div>
+    <div className=" ">
+            <h1 className=" text-xl md:text-2xl  text-gray-900 dark:text-slate-100 text-center">
+             Our Partners 
+            </h1>
+            <div className='px-10 sm:px-20  md:px:36  lg:px-40 py-10'>
+            <p className='text-black dark:text-white text-center'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <div className='flex'>
+<div className='h-20 w-full flex flex-1 m-5 bg-black dark:bg-white'></div>
+<div className='h-20 w-full flex flex-1 m-5 bg-black dark:bg-white'></div>
+<div className='h-20 w-full flex flex-1 m-5 bg-black dark:bg-white'></div>
+<div className='h-20 w-full flex flex-1 m-5 bg-black dark:bg-white'></div>
+            </div>
+            <div className='flex'>
+<div className='h-20 w-full flex flex-1 m-5 bg-black dark:bg-white'></div>
+<div className='h-20 w-full flex flex-1 m-5 bg-black dark:bg-white'></div>
+<div className='h-20 w-full flex flex-1 m-5 bg-black dark:bg-white'></div>
+<div className='h-20 w-full flex flex-1 m-5 bg-black dark:bg-white'></div>
+            </div>
+            </div>
+           
+        </div>
+
+        <div className="flex flex-col md:flex-row bg-slate-200 dark:bg-slate-800 px-10 py-10">
+          <div className='text-center md:text-left'>
+          <h1 className=" text-xl md:text-2xl  text-gray-900 dark:text-slate-100 ">
+             Do you want to volunteer or Be a partner?
+            </h1>
+            <h1 className=" text-xl md:text-2xl  text-gray-900 dark:text-slate-100">
+             Or just simply contact us for more? Let us know!
+            </h1>
+          </div>
+
+            <div className=" flex flex-1 justify-center items-center mt-10 md:mt-0 ">
+            <button className="bg-red-500 px-10 py-3 text-white rounded-lg mr-10">Save Changes</button>
+            <button className="border-solid border-2 border-red-500   px-10 py-3 text-red-500 rounded-lg">Contact us</button>
+            </div>
+            
+           
+        </div>
+        
+    </div>
+</div>
         </div>
    
     </>
